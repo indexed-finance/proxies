482b29c7c1ed29c1b3cf6f3d561cf3a3cceac610
dillon <dillonkellar@protonmail.com>
2020-10-23 19:28:32 -0500

    Enforced solidity version =0.6.12

--- contracts/DelegateCallProxyManager.sol
+++ contracts/DelegateCallProxyManager.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.6.0;
+pragma solidity =0.6.12;
 
 /* ==========  External Libraries  ========== */
 import { Create2 } from "@openzeppelin/contracts/utils/Create2.sol";
